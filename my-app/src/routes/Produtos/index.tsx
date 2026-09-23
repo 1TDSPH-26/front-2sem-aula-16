707dc3ec4f548f2be530e68fadbbe8acdeaa2f3e
marifernandesgm <marifernandesgm17@gmail.com>
2026-09-23 09:17:05 -0300

    feat: Pré listagem da lista de produtos.OK

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,10 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { TipoProduto } from "../../types/types";
 
 export default function Produtos() {
   document.title = "Produtos";
 
   const[produtos, setProdutos] = useState<TipoProduto[]>([]);
+
+  useEffect( ()=>{
+    
+    const carregaProdutos = async () => {
+
+      try {
+        const resposta = await fetch("http://localhost:3001/produtos");
+        
+        if(!resposta.ok) {
+          throw new Error("Erro na listagem dos produtos");
+        }
+
+        const data:TipoProduto[] = await resposta.json();
+        console.log(data);
+
+      } catch (erro) {
+        console.error(erro);
+      }
+
+    }
+
+    carregaProdutos();
+
+  },[]);
 
   return (
     <main>
