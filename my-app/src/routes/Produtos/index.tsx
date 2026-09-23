03238a2327091b27c135a89eb23de1c64b34a4a7
gabefeitosa <gabrielfeitosa321@outlook.com>
2026-09-23 09:45:02 -0300

    feat: tabela de produtos contendo os objetos criado

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -4,30 +4,63 @@ import type { TipoProduto } from "../../types/types";
 export default function Produtos() {
   document.title = "Produtos";
 
-  const[produtos, setProdutos] = useState<TipoProduto[]>([]);
+  const[produtos,setProdutos] = useState<TipoProduto[]>([]);
 
-    useEffect( ()=>{
+  useEffect( ()=>{
 
     const carregaProdutos = async ()=>{
-      try {
-        const response = await fetch("http://localhost:3001/produtos");
 
-        if (!response.ok){
-          throw new Error("Erro na listagem dos produtos!")
+      try {
+        const resposta = await fetch("http://localhost:3001/produtos");
+        
+        if(!resposta.ok){
+          throw new Error("Erro na listagem dos produtos!");
         }
 
-        const data: TipoProduto[] = await response.json();
-        console.log(data);
+        const data:TipoProduto[] = await resposta.json();
+        setProdutos(data);
+
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
     }
+
     carregaProdutos();
+
   },[]);
 
   return (
     <main>
         <h2>Produtos</h2>
+        <div>
+          <table border={1} style={{borderCollapse:"collapse"}}>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NOME</th>
+                <th>PREÇO</th>
+                <th>ESTOQUE</th>
+                <th>AÇÕES</th>
+              </tr>
+            </thead>
+            <tbody>
+              {produtos.map( (produto)=>(
+                <tr key={produto.id}>
+                  <td>{produto.id}</td>
+                  <td>{produto.nome}</td>
+                  <td>{produto.preco}</td>
+                  <td>{produto.estoque}</td>
+                  <td>EDITAR/EXCLUIR</td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colSpan={5}>Quantidade de produtos: {produtos.length}</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
     </main>
   )
 }
