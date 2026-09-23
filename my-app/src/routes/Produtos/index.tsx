3f947159973346b63ba426b53490356d47a6babb
marifernandesgm <marifernandesgm17@gmail.com>
2026-09-23 09:35:29 -0300

    feat: GetAllProdutos completo, utilizando o fetch api com tratamento de erro e renderização em componentes.

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -18,7 +18,7 @@ export default function Produtos() {
         }
 
         const data:TipoProduto[] = await resposta.json();
-        console.log(data);
+        setProdutos(data);
 
       } catch (erro) {
         console.error(erro);
@@ -33,6 +33,36 @@ export default function Produtos() {
   return (
     <main>
         <h2>Produtos</h2>
+        <div>
+          <table border={1} style={{borderCollapse: "collapse"}}>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NOME</th>
+                <th>PREÇO</th>
+                <th>ESTOQUE</th>
+                <th>AÇÕES</th>
+              </tr>
+            </thead>
+            <tbody>
+              {produtos.map( (produto)=>(
+                <tr key={produto.id}>
+                  <td>{produto.id}</td>
+                  <td>{produto.nome}</td>
+                  <td>{produto.preco}</td>
+                  <td>{produto.estoque}</td>
+                  <td>EDITAR/EXCLUIR</td>
+                </tr>
+              ))}
+
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colSpan={5}>Quantidade de produtos: {produtos.length}</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
     </main>
   )
 }
