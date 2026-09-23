714fb965fdd40c9e19f173d99bb40fd549d8c940
gabefeitosa <gabrielfeitosa321@outlook.com>
2026-09-23 09:46:32 -0300

    feat: iniciando o processo de exclusão

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -50,7 +50,7 @@ export default function Produtos() {
                   <td>{produto.nome}</td>
                   <td>{produto.preco}</td>
                   <td>{produto.estoque}</td>
-                  <td>EDITAR/EXCLUIR</td>
+                  <td>EDITAR/<button>Excluir</button></td>
                 </tr>
               ))}
             </tbody>
