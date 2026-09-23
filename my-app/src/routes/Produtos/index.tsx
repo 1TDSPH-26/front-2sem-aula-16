ab82344d231f342d00ac3f357afeeac297118e59
Enzo Nukui <enzonukui2007@gmail.com>
2026-09-23 09:46:18 -0300

    iniciando o processo de exclusão

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -50,7 +50,7 @@ export default function Produtos(){
                                     <td>{produto.nome}</td>
                                     <td>{produto.preco}</td>
                                     <td>{produto.estoque}</td>
-                                    <td>EDITAR/EXCLUIR</td>
+                                    <td>EDITAR/ <button>EXCLUIR</button></td>
                                 </tr>
                             ))}
                         </tbody>
