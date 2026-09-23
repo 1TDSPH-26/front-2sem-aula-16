f64bada2112f83e51d582975d59f441fb73f77f6
Gizetto61 <gigiozetto@gmail.com>
2026-09-23 09:17:40 -0300

    feat: Pré listagem da lista de produtos.

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,10 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { TipoProduto } from "../../Types/types";
 
 export default function Produtos(){
     document.title = "Produtos";
     
     const[produtos, setProdutos] = useState<TipoProduto[]>([]);
+
+    useEffect( () => {
+        const carregaProdutos = async ()=>{
+            try{
+                const resposta = await fetch("http://localhost:3001/produtos");
+
+                if (!resposta.ok){
+                    throw new Error("Erro na listagem dos produtos!");
+                }
+
+                const data:TipoProduto[] = await resposta.json();
+
+                console.log(data);
+            } catch (error){
+                console.error(error);
+            }
+        }
+
+        carregaProdutos();
+
+    }, []);
 
     return(
         <main>
