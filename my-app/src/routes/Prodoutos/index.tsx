b8193648fee8b1ac2ebbaa82f1411c9e5c1cef6e
Pedro-H-Salvatore <pedrosalvatore2112@gmail.com>
2026-09-23 08:44:21 -0300

    feat: Criando o um tipo reutilizavel.

--- my-app/src/routes/Prodoutos/index.tsx
+++ my-app/src/routes/Prodoutos/index.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react"
+import type { tipoProduto } from "../../types/tipoProduto"
+
+
 
 
 export default function Produtos() {
+    const [produto, setProduto] = useState<tipoProduto[]>([])
+
 
     return(
         <div>
---
+++ my-app/src/types/tipoProduto.ts
@@ -0,0 +1,6 @@
+export type tipoProduto = {
+"id": string,
+"nome": string,
+"preco": number,
+"estoque": number,
+}
