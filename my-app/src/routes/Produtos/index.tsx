80a52f52fda09d1504199921c64bbe8a362896d4
felipelima2005 <felipelimaoliveira2008@gmail.com>
2026-09-23 08:43:51 -0300

    feat: TipoProduto criado e pronto para reutilização

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
+import type { TipoProduto } from "../../types/types";
+
 
 export default function Produtos() {
   document.title = "Produtos";
+
+  const[produtos,setProdutos] = useState<TipoProduto[]>([]);
+
   return (
     <main>
         <h2>Produtos</h2>
---
+++ my-app/src/types/types.ts
@@ -0,0 +1,7 @@
+export type TipoProduto = {
+
+    id: string;
+    nome:string;
+    preço:number;
+    estoque:number;
+}
